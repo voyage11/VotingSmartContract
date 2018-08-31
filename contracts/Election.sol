95f49b24e9d5dd3bc4fb6ced79ab7b9f3ca53297
Ricky <ricky@chimaera.my>
2018-08-31 18:19:24 +0800

    - Added function and test

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,8 +1,28 @@
 pragma solidity ^0.4.19;
 
 contract Election {
-	string public candidate;
-	constructor() public  {
-		candidate = "Candidate 1";
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
 	}
+	mapping(uint => Candidate) public candidates;
+	uint public candidatesCount;
+	
+	constructor() public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+
+
+
+
+
+
 }
